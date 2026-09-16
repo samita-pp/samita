d9ed9d19e93bfc7d460704bd2f003fe0a0364d3f
samita-pp <smitanakhpraserith@gmail.com>
2026-09-16 11:21:28 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,14 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-score = int(input())
+score = int(input("คะแนนสอบ: ")
+    if score >=80:
+    print ("A")
+    elif score >=70: 
+    print ("B")
+    elif score >=60: 
+    print ("C")
+    elif score >=50: 
+    print ("D")
+    else 
+    print ("F")
